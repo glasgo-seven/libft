2be5f779084d9bc1c9f3e17da52275ac3afcd9e4
Dimitry <glasgo-seven@users.noreply.github.com>
2020-05-23 21:43:41 +0300

    sh it

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: sanakin <sanakin@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 15:10:40 by sanakin           #+#    #+#             */
-/*   Updated: 2020/05/23 19:41:01 by sanakin          ###   ########.fr       */
+/*   Updated: 2020/05/23 21:40:46 by sanakin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-	t_list	*next;
-
-	next = (*lst)->next;
 	del((*lst)->content);
 	if ((*lst)->next)
-		ft_lstclear((*lst)->next, del);
+		ft_lstclear(&((*lst)->next), del);
 	free(*lst);
 	*lst = NULL;
 }
